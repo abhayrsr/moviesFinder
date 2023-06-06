25111c3e239d1b8e24172d5605271745dbfbb310
abhay <abhayrajsinghrana123@gmail.com>
2023-06-06 17:35:07 +0530

    feat: api linking

---
+++ script.js
@@ -0,0 +1,55 @@
+const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=b8f81ed3537c08aeec4c7063ef1492d5`;
+const imgPath = `https://image.tmdb.org/t/p/w1280`;
+const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=b8f81ed3537c08aeec4c7063ef1492d5`;
+
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const main = document.getElementById('main');
+
+getMovies(apiUrl)
+async function getMovies(url){
+    const res =  await fetch(url);
+    const data = await res.json();
+    displayMovies(data.results);
+    console.log(data.results);
+}
+
+function displayMovies(movies){
+    main.innerHTMl = '';
+    movies.forEach((movie) => {
+        const {title, poster_path, vote_average, overview} = movie;
+        const moviesElement = document.createElement('div');
+        moviesElement.classList.add('movie');
+        moviesElement.innerHTML = `
+        <img src = "${imgPath + poster_path}" alt = "${title}" >
+        <div class = 'movie-info'>
+        <h3>${title}</h3>
+        <span class = "${getClassesByRating(vote_average)}"> ${vote_average} </span>
+        <div class = 'overview'>
+        <h3> Overview </h3>
+        ${overview}
+        </div>
+        </div>
+        `
+        main.appendChild(moviesElement);
+    })
+}
+function getClassesByRating(rating){
+    if(rating >= 8){
+        return 'green'
+    } else if(rating >= 5){
+        return 'orange'
+    } else {
+        return 'red'
+    }
+}
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    searchValue = search.value;
+    if(searchValue && searchValue !== ''){
+        getMovies(searchUrl+searchValue)
+        searchValue = ''
+    } else {
+        window.location.reload();
+    }
+})
